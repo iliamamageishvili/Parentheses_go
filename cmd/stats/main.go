ce3edfea49b25d6a261d2cacb6d053a16ea607b9
Ilia <iliamamageishvili@gmail.com>
2023-04-10 19:59:39 +0400

    modify validation

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -5,29 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
-	"time"
 )
 
 func main() {
-	length := flag.Int("length", 2, "Length of generated sequences (2, 4, or 8)")
+	length := flag.Int("length", 2, "Length of generated sequences")
 	iterations := flag.Int("iterations", 1000, "Number of iterations to run")
 
 	serverHost := flag.String("server", "http://localhost:8080", "URL of the server to use")
 	flag.Parse()
 
-	validLengths := []int{2, 4, 8}
-	validLength := false
-
-	for _, l := range validLengths {
-		if *length == l {
-			validLength = true
-			break
-		}
-	}
-	if !validLength {
-		fmt.Println("Invalid length, must be one of 2, 4, or 8")
+	if *length <= 0 {
+		fmt.Println("Invalid length, must be positive")
 		return
 	}
 
@@ -35,8 +24,6 @@ func main() {
 		fmt.Println("Invalid number of iterations, must be positive")
 		return
 	}
-
-	rand.Seed(time.Now().Unix())
 
 	balancedCount := 0
 	for i := 0; i < *iterations; i++ {
@@ -62,8 +49,3 @@ func main() {
 	percentBalanced := float64(balancedCount) / float64(*iterations) * 100
 	fmt.Printf("Length %d: %.2f%% balanced\n", *length, percentBalanced)
 }
-
-/* commands to test main
-go run cmd/stats/main.go -length 2 -iterations 1000 -server http://localhost:8080
-go run cmd/stats/main.go -length 4 -iterations 1000 -server http://localhost:8080
-go run cmd/stats/main.go -length 8 -iterations 1000 -server http://localhost:8080 */
